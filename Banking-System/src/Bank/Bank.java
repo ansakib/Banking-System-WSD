497da7e67aab95cd250f8c71e30b5f1308b6be8e
ansakib <1705107@ugrad.cse.buet.ac.bd>
2024-01-31 01:10:28 +0600

    Singleton pattern setup for Bank class

---
+++ Banking-System/src/Bank/Bank.java
@@ -0,0 +1,24 @@
+package Bank;
+
+// Singleton class
+
+import Accounts.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private static Bank bankInstance;
+    private List<Account> accounts;
+
+    private Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    public static Bank getInstance() {
+        if (bankInstance == null) {
+            bankInstance = new Bank();
+        }
+        return bankInstance;
+    }
+}
